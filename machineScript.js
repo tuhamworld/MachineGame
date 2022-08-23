41497b3d865a886f6056eefea5dada115ff3fea9
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2022-08-23 06:35:30 +0100

    Add files via upload

---
+++ machineScript.js
@@ -0,0 +1,45 @@
+let response;
+let score = 75;
+let machineActive = true;
+
+if (machineActive) {
+  switch (machineActive = true) {
+    case score < 0 || score > 100:
+      response = "This is not possible, an error has occurred.";
+      break;
+
+    case score === 0 || score <= 19:
+      response = "That was a terrible score — total fail!";
+      break;
+
+    case score == 20 || score <= 39:
+      response =
+        "You know some things, but it's a pretty bad score. Needs improvement.";
+      break;
+
+    case score == 40 || score <= 69:
+      response = "You did a passable job, not bad!";
+      break;
+
+    case score == 70 || score <= 89:
+      response = "That's a great score, you really know your stuff.";
+      break;
+
+    case score == 90 || score <= 100:
+      response = "What an amazing score! Did you cheat? Are you for real?";
+      break;
+  }
+} else {
+  response = "The machine is turned off. Turn it on to process your score.";
+}
+
+const section = document.querySelector("section");
+
+let para1 = document.createElement("p");
+let para2 = document.createElement("p");
+
+para1.textContent = `Your score is ${score}.`;
+para2.textContent = response;
+
+section.appendChild(para1);
+section.appendChild(para2);
